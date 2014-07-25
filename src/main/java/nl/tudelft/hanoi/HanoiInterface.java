77ad92032b3485ee6dba84b8dd318b6921f8c43d
Sander van den Oever <sandervdo@gmail.com>
2014-07-25 21:02:06 +0200

    Removed IntelliJ file from VCS, renamed Interface and modified pom.xml

--- src/main/java/nl/tudelft/hanoi/HanoiInterface.java
+++ src/main/java/nl/tudelft/hanoi/HanoiInterface.java
@@ -1,25 +1,25 @@
-package eishanoi;
+package nl.tudelft.hanoi;
 
-import eis.*;
+import eis.eis2java.environment.AbstractEnvironment;
 import eis.exceptions.*;
 import eis.iilang.Action;
 import eis.iilang.Identifier;
 import eis.iilang.Percept;
 import hanoi.gui.Towers;
-import org.omg.CORBA.Environment;
-
-import java.util.LinkedList;
 
 /**
  * Provides an interface to the Hanoi Tower game.
  *
  * @author Sander van den Oever
  */
-public class HanoiEnvironmentInterface extends EIDefaultImpl {
+public class HanoiInterface extends AbstractEnvironment {
 
 	private Towers game = null;
 
-	public HanoiEnvironmentInterface() {
+    /**
+     * Constructor for the Hanoi Interface.
+     */
+	public HanoiInterface() {
 		// Create a Game instance
 		game = new Towers("Testing through EIS");
 		try {
@@ -27,10 +27,6 @@ public class HanoiEnvironmentInterface extends EIDefaultImpl {
 		} catch(EntityException e) {
 			e.printStackTrace();
 		}
-	}
-
-	public static void main(String[] args) {
-		System.out.println("Hello World!");
 	}
 
 	/**
@@ -41,7 +37,7 @@ public class HanoiEnvironmentInterface extends EIDefaultImpl {
 	 * @param p
 	 * 		pin to add the disc to.
 	 */
-	public void actionaddDisc(int disc, int p) {
+	public void actionAddDisc(int disc, int p) {
 		game.addDisc(disc, p);
 		Percept percept = new Percept("add", new Identifier(""), new Identifier(""));
 		try {
@@ -59,7 +55,7 @@ public class HanoiEnvironmentInterface extends EIDefaultImpl {
 	 * @param to
 	 * 		pin to move the disc to.
 	 */
-	public void actionmoveDisc(int disc, int to) {
+	public void actionMoveDisc(int disc, int to) {
 		game.moveDisc(disc, to);
 		Percept percept = new Percept("move", new Identifier(""), new Identifier(""));
 		try {
@@ -70,34 +66,13 @@ public class HanoiEnvironmentInterface extends EIDefaultImpl {
 	}
 
 	/**
-	 * Gets all percepts of an entity.
-	 * <p/>
-	 * This method must be overridden.
-	 *
-	 * @param entity
-	 * 		is the entity whose percepts should be retrieved.
-	 *
-	 * @return a list of percepts.
-	 */
-	@Override
-	protected LinkedList<Percept> getAllPerceptsFromEntity(String entity) throws PerceiveException, NoEnvironmentException {
-		LinkedList<Percept> percepts = new LinkedList<Percept>();
-
-		if (game == null) throw new NoEnvironmentException("Environment not started!");
-
-
-
-		return percepts;
-	}
-
-	/**
 	 * Returns true if the action is supported by the environment.
 	 *
 	 * @return true if the action is supported by the environment
 	 */
 	@Override
 	protected boolean isSupportedByEnvironment(Action action) {
-		return false;
+		return true;
 	}
 
 	/**
@@ -107,32 +82,6 @@ public class HanoiEnvironmentInterface extends EIDefaultImpl {
 	 */
 	@Override
 	protected boolean isSupportedByType(Action action, String type) {
-		return false;
-	}
-
-	/**
-	 * Returns true if the action is supported by the entity.
-	 *
-	 * @return true if action supported by entity.
-	 */
-	@Override
-	protected boolean isSupportedByEntity(Action action, String entity) {
-		return false;
-	}
-
-	/**
-	 * @return Percept that is result of the action.
-	 */
-	@Override
-	protected Percept performEntityAction(String entity, Action action) throws ActException {
-		return null;
-	}
-
-	/**
-	 * Returns the EIS-runtime-version that is compatible.
-	 */
-	@Override
-	public String requiredVersion() {
-		return null;
-	}
+        return true;
+    }
 }
