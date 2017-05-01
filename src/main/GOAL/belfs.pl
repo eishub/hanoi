1aa77f1b5f5e8bd9d25e022257e03080d090af86
Wouter <w.pasman@tudelft.nl>
2017-05-01 10:39:50 +0200

    GOAL-3000 hanoi GOAL example program changed to use new GOAL grammar

---
+++ src/main/GOAL/belfs.pl
@@ -0,0 +1,3 @@
+% Hanoi game has 3 pins (pin 0, pin 1, pin 2).
+pins(2). 
+
---
+++ src/main/GOAL/know.pl
@@ -0,0 +1,10 @@
+% on(Name, Position, Next)
+:- dynamic on/3.
+
+%disc(Name, size).
+:- dynamic disc/2.
+
+% Pin is empty
+empty(Pin) :- not(on(_, Pin, _)).
+% Disc on pin is top disc (nothing on top of it)
+topDisc(Disc, Pin, Size) :- on(Disc, Pin, _), disc(Disc, Size), not(on(_, Pin, Disc)).
