0e949facd446504cbe5148645164bac21215caf7
Wouter <w.pasman@tudelft.nl>
2017-12-14 16:40:25 +0100

    ENV-1331 hanoi fixes -> 1.1.0-SNAPSHOT

--- src/main/java/hanoi/Hanoi.java
+++ src/main/java/hanoi/Hanoi.java
@@ -1,36 +1,37 @@
 package hanoi;
 
-import hanoi.gui.Towers;
-
 import java.util.ArrayList;
+
+import eis.exceptions.ActException;
+import hanoi.gui.Towers;
 
 /**
  * Simple testing class.
  */
 public class Hanoi {
 
-	public static void main(String[] args) {
-	    Towers window = new Towers("The Hanoi Puzzle", null);
-        window.setVisible(true);
+	public static void main(String[] args) throws ActException {
+		Towers window = new Towers("The Hanoi Puzzle", null);
+		window.setVisible(true);
 
-        ArrayList<Integer> list = new ArrayList<Integer>();
-        list.add(0);
-        list.add(0);
-        list.add(0);
-        list.add(1);
-        
-        window.reset(list);
+		ArrayList<Integer> list = new ArrayList<Integer>();
+		list.add(0);
+		list.add(0);
+		list.add(0);
+		list.add(1);
 
-        window.printDiscs();
-        window.moveDisc(3,1);
-        window.printDiscs();
-        window.moveDisc(3,2);
-        window.printDiscs();
-        window.moveDisc(2,1);
-        window.printDiscs();
-        window.moveDisc(3,1);
-        window.printDiscs();
-        window.moveDisc(4,2);
-        window.printDiscs();
+		window.reset(list);
+
+		window.printDiscs();
+		window.moveDisc(3, 1);
+		window.printDiscs();
+		window.moveDisc(3, 2);
+		window.printDiscs();
+		window.moveDisc(2, 1);
+		window.printDiscs();
+		window.moveDisc(3, 1);
+		window.printDiscs();
+		window.moveDisc(4, 2);
+		window.printDiscs();
 	}
 }
